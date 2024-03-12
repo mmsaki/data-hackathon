f1d206b511dfb9b534f48ec1735ad900f8874f44
Meek Msaki <meek10x@gmail.com>
2024-03-12 03:33:09 -0500

    creating reusable box component

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import styles from "./styles";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>Open up App.js to start working on your app!</Text>
-      </View>
-      <StatusBar style='auto' />
+      <Box>#1</Box>
+      <Box>#2</Box>
+      <Box>#3</Box>
     </View>
   );
 }
